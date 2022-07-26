7c36639234e6428acf71efafe5e4d19b87934aef
Ezra Hampton <ezra.hamptoniii@gmail.com>
2022-07-26 12:35:50 -0400

    making changes to the kids code

--- magic.js
+++ magic.js
@@ -1,1 +1,10 @@
+// where we will store our answers for the magic 8 ball.
 
+const answers = ['Yes', 'no', 'maybe', 'ask again', 'in the near future'];
+
+let message = document.getElementById('message');
+
+function foo() { document.getElementById("message").innerHTML = answers[Math.floor(Math.random() * 5)];
+                
+             
+}
